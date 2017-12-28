389ebb59205ddaf3fe961d9f954f7b6acde0bc65
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2017-12-28 16:02:29 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as ldf from 'ldf-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // var fragmentsClient = new ldf.FragmentsClient('http://fragments.dbpedia.org/2015/en');
+    var endpoints = ['https://bot-ldf.herokuapp.com/P111111','https://ice-engineer.herokuapp.com/P111111']
+    var fragmentsClient = new ldf.FragmentsClient(endpoints);
+
+    var query = 'PREFIX bot: <https://w3id.org/bot#>\
+    SELECT * \
+    WHERE { \
+        ?s a bot:Space .\
+        ?s <https://example.com/ice#heatingDemand> ?hd .\
+    } LIMIT 100';
+
+    var resultsIterator = new ldf.SparqlIterator(query, { fragmentsClient: fragmentsClient });
+
+    // Display a result every 0.1 seconds
+    setInterval(() => {
+        var result = resultsIterator.read();
+        if (result) console.log(result);
+      }, 100);
+  }
+
 }
